4204f69981f0fbd0fca9adecbd51fc44098e5657
felipeDesenvolvedor <felipedasilvasantos30@gmail.com>
2021-02-25 15:31:46 -0300

    node server

---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+app.use(express.static('assets'));
+
+// viewed at http://localhost:8080
+app.get('/', function(req, res) {
+    console.log(__dirname);
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.get('/posts', function(req, res) {
+    console.log(__dirname);
+    res.sendFile(path.join(__dirname + '/posts/index.html'));
+});
+
+app.listen(3000, () => {
+    console.log('Listen localhost:3000');
+});
